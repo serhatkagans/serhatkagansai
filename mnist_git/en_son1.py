b594c9419e4ae39965eec431656519b8feb821a3
serhatkagans <47238858+serhatkagans@users.noreply.github.com>
2024-09-08 13:33:22 +0300

    Update en_son1.py

--- mnist_git/en_son1.py
+++ mnist_git/en_son1.py
@@ -1,44 +1,35 @@
+import os
 import joblib
-# Modeli tekrar yükleme
-lr_loaded = joblib.load('logistic_regression_mnist_model.pkl')
-print("Model yüklendi.")
-
-# Yeni bir resimle tahmin yapma
 from PIL import Image
 import numpy as np
-
-# Yeni bir resim açma, boyutlandırma ve gri tonlamaya çevirme
-img = Image.open("2.png")  # Resim yolunu güncelleyin
-img = img.resize((28, 28))
-img = img.convert("L")
-
-# Resmi numpy dizisine çevirme ve modelin kabul edeceği boyuta getirme
-img_array = np.array(img).reshape(1, -1)  # 1 satırlık veriye çeviriyoruz
-
-# Modelle tahmin yapma
-pred = lr_loaded.predict(img_array)
-print("Tahmin edilen sınıf:", pred)
-
-# Tahmin edilen rakamı görselleştirme
 import matplotlib.pyplot as plt
-plt.imshow(img_array.reshape(28, 28), cmap="gray")
-plt.title(f"Tahmin edilen rakam {pred[0]}")
-plt.show()
-#%%
 import streamlit as st
+
+# Dinamik olarak modelin dosya yolunu belirleyin
+model_path = os.path.join(os.path.dirname(__file__), 'logistic_regression_mnist_model.pkl')
+
+# Modeli tekrar yükleme
+lr_loaded = joblib.load(model_path)
+st.write("Model yüklendi.")
+
+# Streamlit uygulama başlığı
 st.title("MNIST Rakam Tanıma Uygulaması")
 
-
-uploaded_file=st.file_uploader("lütfen resim yükleyin",type=["png","jpg","jpeg"])
+# Kullanıcıdan resim yüklemesini isteyin
+uploaded_file = st.file_uploader("Lütfen bir resim yükleyin", type=["png", "jpg", "jpeg"])
 
 if uploaded_file is not None:
-    img=Image.open(uploaded_file)
-    img=img.resize((28,28))
-    img=img.convert("L")
+    # Resmi açma ve işleme
+    img = Image.open(uploaded_file)
+    img = img.resize((28, 28))
+    img = img.convert("L")
     
-    img_array=np.array(img).reshape(1,-1)
+    # Resmi numpy dizisine çevirme ve modelin kabul edeceği boyuta getirme
+    img_array = np.array(img).reshape(1, -1)
     
-    pred=lr_loaded.predict(img_array)
-    st.write(f"tahmin edilen sınıf: {pred[0]}")
+    # Modelle tahmin yapma
+    pred = lr_loaded.predict(img_array)
+    st.write(f"Tahmin edilen sınıf: {pred[0]}")
     
-    st.image(img,caption=f"tahmin edilen rakam: {pred[0]}")
+    # Tahmin edilen rakamı görselleştirme
+    st.image(img, caption=f"Tahmin edilen rakam: {pred[0]}")
